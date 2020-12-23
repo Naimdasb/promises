54f5770cda50c561432271685607c276395bfc12
naimdasb <naimdasb@gmail.com>
2020-12-22 21:29:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const prom = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        let isOk = true;
+        if (isOk) {
+            resolve('OK');
+        } else {
+            reject('Wrong 0')
+        }
+    }, 2000)
+})
+
+const prom2 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        let isOk = true;
+        if (isOk) {
+            resolve('OK');
+        } else {
+            reject('Wrong 1')
+        }
+    }, 2000)
+})
+
+
+Promise.all([prom, prom2]).then(res => console.log(res)).catch(err => console.log(err))
